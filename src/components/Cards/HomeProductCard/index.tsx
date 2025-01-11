d76b348518a89f1289b2196801b5a8ef111132eb
Anna Luiza Fistarol <luizafistarol@gmail.com>
2025-01-11 18:00:25 -0300

    feat: add HomeProductCard component for displaying product details

---
+++ src/components/Cards/HomeProductCard/index.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { IProduct } from '@/app/types'
+import FavoriteButton from '@/components/Buttons/FavoriteButton'
+import Link from 'next/link'
+import Image from 'next/image'
+import * as S from './styles'
+
+/**
+ * PRODUCT CARD [Home Page]
+ * @param product
+ * @returns
+ */
+export default function HomeProductCard({ product }: Readonly<{ product: IProduct }>) {
+  const { id, name, description, price, image, promotional_price, discount_percentage } = product
+
+  return (
+    <S.CardWrapper>
+      <S.ImageWrapper>
+        <FavoriteButton productInfo={product} />
+
+        <Link href={`/product/${id}`}>
+          {discount_percentage && (
+            <S.DiscountBadge className="discount-badge">
+              <span>{discount_percentage}% OFF</span>
+            </S.DiscountBadge>
+          )}
+          {image && <Image src={image} alt={name} width={288} height={352} />}
+        </Link>
+      </S.ImageWrapper>
+
+      <Link href={`/product/${id}`}>
+        <S.ProductTitle>{name}</S.ProductTitle>
+
+        <S.Description>
+          <p>{description}</p>
+        </S.Description>
+
+        <S.PriceWrapper>
+          <p className={promotional_price ? 'old-price' : ''}>R$ {price}</p>
+          {product?.promotional_price && <p className="promotional-price">R$ {promotional_price}</p>}
+        </S.PriceWrapper>
+      </Link>
+    </S.CardWrapper>
+  )
+}
---
+++ src/components/Cards/HomeProductCard/styles.ts
@@ -0,0 +1,109 @@
+import styled from 'styled-components'
+
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  color: ${({ theme }) => theme.palette.neutral.dark};
+`
+
+export const ImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  border-radius: 0.25rem;
+  height: 22rem;
+
+  img {
+    object-fit: cover;
+    width: 100%;
+    border-radius: 0.25rem;
+    transition: transform 0.3s;
+
+    &:hover {
+      transform: scale(1.15);
+      border-radius: 0.25rem;
+    }
+  }
+
+  .favorite-button {
+    position: absolute;
+    top: 0.5rem;
+    right: 0.5rem;
+    z-index: 2;
+  }
+
+  .discount-badge {
+    z-index: 2;
+  }
+`
+
+export const DiscountBadge = styled.div`
+  position: absolute;
+  top: 0.5rem;
+  left: 0.5rem;
+  background: ${({ theme }) => theme.palette.primary.default};
+  color: ${({ theme }) => theme.palette.neutral.lighter};
+  font-weight: 600;
+  padding: 0.25rem;
+  border-radius: 0.25rem;
+`
+
+export const ProductTitle = styled.h4`
+  font-size: 1.125rem;
+  font-weight: 700;
+  margin-top: 0.75rem;
+  color: ${({ theme }) => theme.palette.neutral.dark};
+`
+
+export const Description = styled.div`
+  color: ${({ theme }) => theme.palette.neutral.dark};
+
+  p {
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  span {
+    font-weight: 500;
+  }
+`
+
+export const PriceWrapper = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  font-family: var(--font-smooch-sans);
+
+  p {
+    font-size: 1.75rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.palette.neutral.dark};
+    line-height: 1.5;
+  }
+
+  .old-price {
+    text-decoration: line-through;
+    color: ${({ theme }) => theme.palette.primary.default};
+  }
+`
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 1.5rem;
+
+  button {
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
+    background-color: ${({ theme }) => theme.palette.primary.default};
+    color: ${({ theme }) => theme.palette.neutral.lighter};
+    font-size: 1rem;
+    font-weight: 500;
+    cursor: pointer;
+  }
+`
