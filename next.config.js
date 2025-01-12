0e9bf531bb7e8d4ebc3a33dda17987588c253f3d
Anna Luiza Fistarol <luizafistarol@gmail.com>
2025-01-11 22:40:57 -0300

    chore: migrate configuration from next.config.ts to next.config.js and update dependencies

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const nextConfig = {
+  images: {
+    domains: ['s3-sa-east-1.amazonaws.com'],
+  },
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  }
+}
+
+module.exports = nextConfig
--- next.config.ts
+++
@@ -1,7 +0,0 @@
-const nextConfig = {
-  images: {
-    domains: ['s3-sa-east-1.amazonaws.com'],
-  },
-  reactStrictMode: true,
-}
-export default nextConfig
